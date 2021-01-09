9eda098c670e276f7bc549c7fb7975a7cb4facea
EC2 Default User <ec2-user@ip-172-31-94-20.ec2.internal>
2021-01-09 05:27:33 +0000

    [finish] 2-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -34,6 +34,9 @@ class TodolistsController < ApplicationController
 
 
   def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to todolists_path  # 投稿一覧画面へリダイレクト
   end
 
 
